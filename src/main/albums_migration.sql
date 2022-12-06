c9e34671fa4d88521e7bf113c3fb44611ea408b9
Miles Raker <milesraker@gmail.com>
2022-12-06 14:02:13 -0800

    SQL Basic Statements part 1 and 2 complete

--- src/main/albums_migration.sql
+++ src/main/albums_migration.sql
@@ -1,4 +1,8 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
+CREATE USER IF NOT EXISTS 'codeup_test_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON *.* TO 'codeup_test_user'@'localhost';
+/*mysql -u codeup_test_user -p; <- run this line in terminal*/
 DROP TABLE if EXISTS albums;
 CREATE TABLE albums(
   id INT UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -9,3 +13,6 @@ CREATE TABLE albums(
   genre VARCHAR(50),
   PRIMARY KEY (id)
 );
+DESCRIBE albums;
+
+SELECT * FROM albums;
---
+++ src/main/albums_seeder.sql
@@ -0,0 +1,37 @@
+USE codeup_test_db;
+/* add data rows*/
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock');
+#
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop');
+#
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('AC/DC', 'Back in Black', 1980, 30.1, 'Country rock');
+#
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock');
+#
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock'),
+ ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop'),
+ ('AC/DC', 'Back in Black', 1980, 30.1, 'Country rock'),
+ ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
+ ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive rock'),
+       ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country rock'),
+       ('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft rock'),
+       ('Bee Gees', 'Saturday Night Fever', 1977, 22.1, 'Disco'),
+       ('Meat Loaf', 'Bat Out of Hell', 1977, 22, 'Hard rock'),
+       ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative rock'),
+       ('Various artists','Dirty Dancing', 1987, 24.1, 'Pop'),
+       ('Celine Dion', 'Falling into You', 1996, 21.1, 'Pop'),
+       ('Adele', '21', 1996, 27.1, 'Pop'),
+       ('The Beatles', '1', 2000, 23.2, 'Rock'),
+       ('Metallica','Metallica', 2000, 23.2, 'Heavy metal'),
+       ('Celine Dion', 'Let''s Talk about Love', 1997, 19.5, 'Pop');
+ ;
